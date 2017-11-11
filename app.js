880b7148d400dcc10417591bd6e0b2d997479ac6
marcony meza <marconymeza@gmail.com>
2017-11-11 17:40:47 -0600

    agregado el login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var GDB = require('goblindb');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var GDB = require("goblindb");
+
+var goblinDB = GDB();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(goblinDB.get())
   res.render('index', { title: 'Green' });
 });
 
